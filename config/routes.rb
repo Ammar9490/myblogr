01b135729e7f398bb41c4b432ae80b9fa0c1fb75
Ammar Adel <ammar@email.com>
2020-11-13 16:37:31 +0300

    ammr bloger

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  get 'controls/login'
+  # get 'users/login'
+  root "articles#index"
+  
+  resources :articles  do
+    resources :comments
+    end
+  resources :users
+  match ':controller(/:action(/:id))', :via=> [:get,:post]
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
